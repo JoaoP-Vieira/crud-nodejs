52ec972fdadde4129aad9da070839bf9e556636b
João Pedro Vieira <jp_vieira@aol.com>
2022-02-15 20:12:55 -0300

    Setup express

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,12 @@
-console.log('test');
+import express from 'express';
+
+const PORT = 3001;
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello there');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server runing on http://localhost:${PORT}`);
+});
