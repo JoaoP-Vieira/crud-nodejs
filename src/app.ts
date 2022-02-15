cb3c8f7f66a5491f51aadd2bcfa308d1214480c0
João Pedro Vieira <jp_vieira@aol.com>
2022-02-15 20:29:55 -0300

    MySQL connection

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,29 @@
 import express from 'express';
+import mysql from 'mysql';
+import cors from 'cors';
 
 const PORT = 3001;
 const app = express();
+
+const db = mysql.createConnection({
+  user: 'user',
+  host: 'localhost',
+  password: 'password',
+  database: 'database',
+});
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/get', (req, res) => {
+  db.query('SELECT * FROM products', (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send(result);
+    }
+  });
+});
 
 app.get('/', (req, res) => {
   res.send('Hello there');
